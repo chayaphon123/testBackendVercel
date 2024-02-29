dd84c7f0b1c5d11372e13c810ef5d2a1f6af7ff4
PattananNinja <155932481+PattananNinja@users.noreply.github.com>
2024-03-01 03:38:15 +0700

    Delete Backend_Project-main directory

--- Backend_Project-main/routes/coworks.js
+++
@@ -1,9 +0,0 @@
-﻿const express = require ('express');
-const {getCoWorks, getCoWork, postCoWorks, putCoWork, deleteCoWork} = require ('../controllers/coworks');
-
-const router = express.Router();
-
-router.route('/').get(getCoWorks).post(postCoWorks);
-router.route('/:id').get(getCoWork).put(putCoWork).delete(deleteCoWork);
-
-module.exports=router;
