b1b175141c261089352f902f06141eefa77c3458
Chayaphon Kingkaewkanthong <6633046121@student.chula.ac.th>
2024-03-24 15:29:09 +0700

    Fix 1

--- routes/coworks.js
+++ routes/coworks.js
@@ -1,5 +1,43 @@
-﻿const express = require ('express');
+﻿/**
+* @swagger
+* components:
+*   schemas:
+*     CoWork:
+*       type: object
+*       required:
+*         - name
+*         - address
+*         - phonenumber
+*         - openclosetime
+*         - picture
+*       properties:
+*         name:
+*           type: string
+*           description: Name of the co-working space
+*         address:
+*           type: string
+*           description: Address of the co-working space
+*         phonenumber:
+*           type: string
+*           description: Phone number of co-working space
+*         openclosetime:
+*           type: string
+*           description: Open-Close time of co-working space
+*         picture:
+*           type: string
+*           description: Picture of the co-working space
+*/
+
+
+const express = require ('express');
 const {getCoWorks, getCoWork, postCoWorks, putCoWork, deleteCoWork} = require ('../controllers/coworks');
+
+/**
+* @swagger
+* tags:
+*   name: CoWork
+*   description: The co-working spaces managing API
+*/
 
 const router = express.Router();
 
@@ -9,9 +47,74 @@ const {protect,authorize} = require('../middleware/auth');
 const reservationRouter=require('./reservations');
 
 //Re-route into other resource routers
-router.use('/:coworkId/reservations', reservationRouter);
 
+/**
+* @swagger
+* /coworks:
+*   post:
+*     security:
+*       - bearerAuth: []
+*     summary: Create a new co-working space
+*     tags: [CoWork]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/CoWork'
+*     responses:
+*       201:
+*         description: The co-working sapce was successfully created
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/CoWork'
+*       500:
+*         description: Some server error
+*/
+
+/**
+* @swagger
+* /coworks:
+*   get:
+*     summary: Returns the list of all the co-working spaces
+*     tags: [CoWork]
+*     responses:
+*       200:
+*         description: The list of the co-working space
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*               $ref: '#/components/schemas/CoWork'
+*/
+router.use('/:coworkId/reservations', reservationRouter);
 router.route('/').get(getCoWorks).post(protect,authorize('admin'),postCoWorks);
+
+/**
+* @swagger
+* /coworks/{id}:
+*   get:
+*     summary: Get the co-working space by id
+*     tags: [CoWork]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The co-working space id
+*     responses:
+*       200:
+*         description: The co-working space description by id
+*         contents:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/CoWork'
+*       404:
+*         description: The co-working space was not found
+*/
 router.route('/:id').get(getCoWork).put(protect,authorize('admin'),putCoWork).delete(protect,authorize('admin'),deleteCoWork);
 
 module.exports=router;
--- server.js
+++ server.js
@@ -8,54 +8,76 @@ const {xss}=require('express-xss-sanitizer');
 const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const cors=require('cors');
-
-//Load env vars
-dotenv.config({path:'./config/config.env'});
-
-//Connect to database
-connectDB();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const bodyParser = require('body-parser');
 
 //Routes file
 const coworks = require('./routes/coworks');
 const auth = require('./routes/auth');
 const resevations = require('./routes/reservations');
 
-const app=express();
+//Load env vars
+dotenv.config({path:'./config/config.env'});
 
+//Connect to database
+connectDB();
+const app=express();
+//Enable CORS
+app.use(cors());
 //Body parser
 app.use(express.json());
-app.use(cookieParser());
-
 //Sanitize data
 app.use(mongoSanitize());
-
 //Set security headers
 app.use(helmet());
-
 //Prevent XSS attacks
 app.use(xss());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 //Rate Limiting
 const limiter=rateLimit({
     windowsMs:10*60*1000,//10 mins
     max: 500
 });
 app.use(limiter);
-
 //Prevent http param pollutions
 app.use(hpp());
 
-//Enable CORS
-app.use(cors());
-
-
+app.use(cookieParser());
 app.use('/api/v1/coworks', coworks);
 app.use('/api/v1/auth',auth);
 app.use('/api/v1/reservations', resevations);
 
 const PORT=process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(
+    PORT, 
+    console.log('Server running in ', 
+    process.env.NODE_ENV, 
+    ' mode on ' +
+    process.env.HOST + 
+    " " + PORT)
+);
 
+const swaggerOptions={
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'Backend API'
+        },
+        servers: [
+            {
+                url: process.env.HOST + ':' + PORT + '/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err,promise) => {
