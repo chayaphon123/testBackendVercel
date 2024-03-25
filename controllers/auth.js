0c519860145f11ff2401fe5afc688850765d817a
Chayaphon Kingkaewkanthong <6633046121@student.chula.ac.th>
2024-03-25 18:15:11 +0700

    add role to token

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,6 +18,7 @@ const sendTokenResponse = (user, statusCode, res) =>{
         _id: user._id,
         name: user.name,
         email: user.email,
+        role: user.role,
         token
     });
 }
